23ad4070f890c247e182a0175c59d348ff34e8ca
fr1day <bmahmudov321@gmail.com>
2021-12-23 17:42:37 +0500

    Migrate qlindi

--- YouTubeDrf/YouTubeDrf/settings.py
+++ YouTubeDrf/YouTubeDrf/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'YouTuabeApp',
+    'YoutubeApp',
     'rest_framework',
 ]
 
